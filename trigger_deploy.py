51e7de24cf7629f313109de585e4823e332bf2ed
millen1m <maxim.millen@gmail.com>
2018-03-20 13:28:11 +0000

    removed need for pypi branch.

--- trigger_deploy.py
+++ trigger_deploy.py
@@ -7,4 +7,4 @@ with open("eqsig/__about__.py") as fp:
 version = about['__version__']
 
 subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
-subprocess.check_call(["git", "push", "--tags", "origin", "pypi"])
+subprocess.check_call(["git", "push", "--tags", "origin", "master"])
