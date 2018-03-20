97a8444d005fbb406e57ce688f2d856cb5b5a73b
millen1m <maxim.millen@gmail.com>
2018-03-20 11:35:35 +0000

    added trigger_deploy script and fixed issue with finding requirements file for deployment.

---
+++ trigger_deploy.py
@@ -0,0 +1,10 @@
+import subprocess
+
+about = {}
+with open("__about__.py") as fp:
+    exec(fp.read(), about)
+
+version = about['__version__']
+
+subprocess.check_call(["git", "tag", version, "-m", "version %s" % version])
+subprocess.check_call(["git", "push", "--tags", "origin", "pypi"])
