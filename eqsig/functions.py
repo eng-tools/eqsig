d77f53a6f4225a59e55b916e9cd784916f756cfc
millen1m <maxim.millen@gmail.com>
2018-08-10 13:00:34 +0100

    cleaned version of peak sum function seems to be working.

--- eqsig/functions.py
+++ eqsig/functions.py
@@ -27,6 +27,31 @@ def determine_peaks_only_series(values):
 
 
 def determine_delta_peak_only_series(values):
+    values = np.array(values)
+    values -= values[0]
+    peak_indices = determine_indices_of_peaks(values)
+    peak_values = np.take(values, peak_indices)
+    # peak_values = np.insert(peak_values, 0, 0)
+    diff_peaks = np.diff(peak_values)
+    diff_peaks = np.insert(diff_peaks, len(diff_peaks), 0)
+    cum_peaks = np.cumsum(diff_peaks)
+    # print(np.cumsum(np.abs(diff_peaks)) - np.mean(peak_values))
+    # diff_peaks = np.insert(diff_peaks, 0, peak_values[0] - values[0])  # insert first peak from start
+    # must switch signs otherwise doesn't count
+    signs = np.where(np.mod(np.arange(len(peak_values)), 2), 1, -1)
+    delta_peaks = np.where(-signs * peak_values < 0, -np.abs(peak_values), np.abs(peak_values))
+    # delta_peaks = delta_peaks[1:]
+    # abs_diff_peaks = np.abs(diff_peaks)
+    # delta_peaks = abs_diff_peaks[1:] - abs_diff_peaks[:-1]  # remove the restoring part
+    # delta_peaks = np.insert(delta_peaks, 0, diff_peaks[0])
+    # delta_peaks = np.cumsum(abs_diff_peaks)
+    delta_peaks = cum_peaks
+    delta_peaks_series = np.zeros_like(values)
+    np.put(delta_peaks_series, peak_indices, delta_peaks)
+
+    return delta_peaks_series
+
+def determine_delta_peak_only_series_4_cleaned_data(values):
     peak_indices = determine_indices_of_peaks(values)
     peak_values = np.take(values, peak_indices)
     # peak_values = np.insert(peak_values, 0, 0)
@@ -48,15 +73,38 @@ def determine_delta_peak_only_series(values):
     return delta_peaks_series
 
 
+def determine_delta_peak_only_series_cleaned(values):
+    # remove all non-changing values
+    diff_values = np.diff(values)
+    # diff_values = np.insert(diff_values, len(diff_values), -values[-1])
+    non_zero_indices = np.where(diff_values != 0)[0]
+    cleaned_values = np.take(values, non_zero_indices)
+    cleaned_values = np.insert(cleaned_values, len(cleaned_values), values[-1])
+    cleaned_delta_peak_series = determine_delta_peak_only_series_4_cleaned_data(cleaned_values)
+    delta_peaks_series = np.zeros_like(values)
+    np.put(delta_peaks_series, non_zero_indices, cleaned_delta_peak_series)
+    return delta_peaks_series
+
+
+
 
 def test_determine_peaks_only_series_with_a_double_peak():
-    values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0]) + 0
+    values = np.array([0, 1, 1, -3, -5, 0]) * 1  # constant then reverse
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = determine_delta_peak_only_series(values)
+    # peaks_only = determine_delta_peak_only_series_4_cleaned_data(values)
+    peaks_only = determine_delta_peak_only_series_cleaned(values)
     cum_peaks = np.sum((peaks_only))
     print(cum_peaks, expected_sum)
-    assert np.isclose(cum_peaks, expected_sum)
+    assert np.isclose(cum_peaks, expected_sum), 1
+    values = np.array([0, 1, 1, 3, -5, 0]) * 1  # constant the no reverse
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    expected_sum = cum_abs_delta_values / 2
+    # peaks_only = determine_delta_peak_only_series_4_cleaned_data(values)
+    peaks_only = determine_delta_peak_only_series_cleaned(values)
+    cum_peaks = np.sum((peaks_only))
+    print(cum_peaks, expected_sum)
+    assert np.isclose(cum_peaks, expected_sum), 2
 
 
 if __name__ == '__main__':
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -25,20 +25,45 @@ def test_determine_peaks_only_series_with_sine_wave():
     assert np.isclose(cum_peaks, expected_sum)
 
 
-def test_determine_peaks_only_series_with_ground_motion():
+def test_determine_peaks_only_series_with_ground_motion_1():
     record_path = TEST_DATA_DIR
     record_filename = 'test_motion_dt0p01.txt'
     rec = np.loadtxt(record_path + record_filename)
+    rec = rec[:-1] * -1000
+    rec[-1] = rec[0]
     cum_abs_delta_values = np.sum(np.abs(np.diff(rec)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(rec)
-    cum_peaks = np.sum(np.abs(peaks_only))
+    peaks_only = eqsig.determine_delta_peak_only_series_cleaned(rec)
+    cum_peaks = np.sum(peaks_only)
     print(cum_peaks, expected_sum)
     assert np.isclose(cum_peaks, expected_sum)
 
 
+def test_determine_peaks_only_series_with_ground_motion():
+    record_path = TEST_DATA_DIR
+    record_filename = 'test_motion_dt0p01.txt'
+    rec = np.loadtxt(record_path + record_filename)
+    rec = rec[:-1] * -1000
+    rec[-1] = rec[0]
+    cum_abs_delta_values = np.sum(np.abs(np.diff(rec)))
+    expected_sum = cum_abs_delta_values / 2
+    peaks_only = eqsig.determine_delta_peak_only_series_cleaned(rec)
+    cum_peaks = np.sum(peaks_only)
+    print(cum_peaks, expected_sum)
+    assert np.isclose(cum_peaks, expected_sum)
+
+
+def test_determine_peaks_only_series_with_a_double_peak():
+    values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0]) + 4
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    expected_sum = cum_abs_delta_values / 2
+    peaks_only = eqsig.determine_delta_peak_only_series_cleaned(values)
+    cum_peaks = np.sum((peaks_only))
+    print(cum_peaks, expected_sum)
+    assert np.isclose(cum_peaks, expected_sum)
+
 
 if __name__ == '__main__':
-    test_determine_peaks_only_series_with_sine_wave()
-    test_determine_peaks_only_series_with_triangle_series()
-    # test_determine_peaks_only_series_with_ground_motion()
+    # test_determine_peaks_only_series_with_sine_wave()
+    # test_determine_peaks_only_series_with_triangle_series()
+    test_determine_peaks_only_series_with_ground_motion_1()
