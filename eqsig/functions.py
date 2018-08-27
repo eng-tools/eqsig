1524ac7a413cdd3143f36c6c5af178f7e16f631f
millen1m <maxim.millen@gmail.com>
2018-08-27 15:48:01 +0100

    refining energy series methods.

--- eqsig/functions.py
+++ eqsig/functions.py
@@ -9,12 +9,22 @@ def time_series_from_motion(motion, dt):
 
 
 def determine_indices_of_peaks(values):
+    """
+    Determines the position of values that form a local peak in a signal.
+
+    :param values:
+    :return:
+    """
     diff = np.diff(values)
     # if negative then direction has switched
     direction_switch = diff[1:] * diff[:-1]
     direction_switch = np.insert(direction_switch, 0, 0)
-    peak_indices = np.where(direction_switch < 0)
-    return peak_indices[0]
+    peaks = np.where(direction_switch < 0)
+    peak_indices = peaks[0]
+    peak_indices = np.insert(peak_indices, 0, 0) # Include first and last value
+    peak_indices = np.insert(peak_indices, len(peak_indices), len(values) - 1)
+
+    return peak_indices
 
 
 # def determine_peaks_only_series(values):
@@ -28,10 +38,18 @@ def determine_indices_of_peaks(values):
 #     return values * peaks
 
 
-def determine_delta_peak_only_series_4_cleaned_data(values):
+def determine_peak_only_series_4_cleaned_data(values):
+    """
+    Determines the
+
+    Note: array must not contain adjacent repeated values
+
+    :param values:
+    :return:
+    """
     peak_indices = determine_indices_of_peaks(values)
     peak_values = np.take(values, peak_indices)
-    signs = np.where(np.mod(np.arange(len(peak_values)), 2), 1, -1)
+    signs = np.where(np.mod(np.arange(len(peak_values)), 2), -1, 1)
     delta_peaks = np.where(-signs * peak_values < 0, -np.abs(peak_values), np.abs(peak_values))
     delta_peaks_series = np.zeros_like(values)
     np.put(delta_peaks_series, peak_indices, delta_peaks)
@@ -39,17 +57,45 @@ def determine_delta_peak_only_series_4_cleaned_data(values):
     return delta_peaks_series
 
 
+def determine_peak_only_delta_series_4_cleaned_data(values):
+    """
+    Determines the
+
+    Note: array must not contain adjacent repeated values
+
+    :param values:
+    :return:
+    """
+    peak_indices = determine_indices_of_peaks(values)
+    peak_values = np.take(values, peak_indices)
+    delta_peaks = np.diff(peak_values)
+    delta_peaks = np.insert(delta_peaks, 0, 0)
+    delta_peaks_series = np.zeros_like(values)
+    assert len(delta_peaks) == len(peak_indices)
+    np.put(delta_peaks_series, peak_indices, delta_peaks)
+
+    return delta_peaks_series
+
+
 def clean_out_non_changing(values):
+    """
+    Takes an array removes all values that are the same as the previous value.
+
+    :param values: array of floats
+    :return: cleaned array, indices of clean values in original array
+    """
     diff_values = np.diff(values)
+    diff_values = np.insert(diff_values, 0, values[0])
     non_zero_indices = np.where(diff_values != 0)[0]
+    non_zero_indices = np.insert(non_zero_indices, 0, 0)
+
     cleaned_values = np.take(values, non_zero_indices)
-    cleaned_values = np.insert(cleaned_values, len(cleaned_values), values[-1])
     return cleaned_values, non_zero_indices
 
 
-def determine_delta_peak_only_series(values):
+def determine_peaks_only_delta_series(values):
     """
-    Creates an array with only the changes in the peak values and zeros for non-peak values.
+    Creates an array with the changes between peak values and zeros for non-peak values.
 
     Parameters
     ----------
@@ -60,7 +106,38 @@ def determine_delta_peak_only_series(values):
     --------
     >>> values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0])
     np.array([0, 2, 1, 2, 0.3, 1, 0.3, -1, 0.4, 1, 0])
-    >>> determine_delta_peak_only_series(values)
+    >>> determine_peaks_only_delta_series(values)
+    array([0,  2, -1,  1,  0,  -1,  0,  -2,  0,  2,  0])
+    """
+    # enforce array type
+    values = np.array(values)
+    # rebase to zero as first value
+    values -= values[0]
+    # remove all non-changing values
+    cleaned_values, non_zero_indices = clean_out_non_changing(values)
+    cleaned_values *= np.sign(cleaned_values[1])  # ensure first value is increasing
+    # compute delta peaks for cleaned data
+    cleaned_delta_peak_series = determine_peak_only_delta_series_4_cleaned_data(cleaned_values)
+    # re-index data to uncleaned array
+    delta_peaks_series = np.zeros_like(values)
+    np.put(delta_peaks_series, non_zero_indices, cleaned_delta_peak_series)
+    return delta_peaks_series
+
+
+def determine_pseudo_cyclic_peak_only_series(values):
+    """
+    Creates an array with only peak values assuming an alternative sign and zeros for non-peak values.
+
+    Parameters
+    ----------
+    :param values: array_like, array of values
+    :return:
+
+    Examples
+    --------
+    >>> values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0])
+    np.array([0, 2, 1, 2, 0.3, 1, 0.3, -1, 0.4, 1, 0])
+    >>> determine_pseudo_cyclic_peak_only_series(values)
     array([0,  2, -1,  2,  0,  1,  0,  1,  0,  1,  0])
     """
     # enforce array type
@@ -71,7 +148,7 @@ def determine_delta_peak_only_series(values):
     cleaned_values, non_zero_indices = clean_out_non_changing(values)
     cleaned_values *= np.sign(cleaned_values[1])  # ensure first value is increasing
     # compute delta peaks for cleaned data
-    cleaned_delta_peak_series = determine_delta_peak_only_series_4_cleaned_data(cleaned_values)
+    cleaned_delta_peak_series = determine_peak_only_series_4_cleaned_data(cleaned_values)
     # re-index data to uncleaned array
     delta_peaks_series = np.zeros_like(values)
     np.put(delta_peaks_series, non_zero_indices, cleaned_delta_peak_series)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,46 +6,99 @@ from eqsig import functions
 from tests.conftest import TEST_DATA_DIR
 
 
-def test_determine_peaks_only_series_with_triangle_series():
+def test_determine_pseudo_cyclic_peak_only_series_with_triangle_series():
     values = [0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0]
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
     cum_peaks = np.sum(np.abs(peaks_only))
     print(cum_peaks, expected_sum)
     assert np.isclose(cum_peaks, expected_sum)
 
 
-def test_determine_peaks_only_series_with_sine_wave():
+def test_determine_peaks_only_delta_series_with_triangle_series():
+    values = [0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0]
+    peaks_only = eqsig.determine_peaks_only_delta_series(values)
+    cum_peaks = np.sum(np.abs(peaks_only))
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    cum_diff = np.sum(peaks_only)
+    assert np.isclose(cum_diff, 0), (cum_diff, 0)
+    assert np.isclose(cum_abs_delta_values, 10), (cum_abs_delta_values, 10)
+
+
+def test_determine_pseudo_cyclic_peak_only_series_with_sine_wave():
     time = np.arange(99)
     values = np.sin(time)
     values[-1] = 0
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
     cum_peaks = np.sum(np.abs(peaks_only))
     assert np.isclose(cum_peaks, expected_sum)
 
 
-def test_determine_peaks_only_series_with_ground_motion():
+def test_determine_peaks_only_delta_series_with_sine_wave():
+    time = np.arange(99)
+    values = np.sin(time)
+    values[-1] = 0
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    expected_sum = cum_abs_delta_values
+    peaks_only = eqsig.determine_peaks_only_delta_series(values)
+    cum_peaks = np.sum(np.abs(peaks_only))
+    assert np.isclose(cum_peaks, expected_sum), (cum_peaks, expected_sum)
+
+
+def test_determine_pseudo_cyclic_peak_only_series_with_ground_motion():
     record_path = TEST_DATA_DIR
     record_filename = 'test_motion_dt0p01.txt'
     rec = np.loadtxt(record_path + record_filename)
     cum_abs_delta_values = np.sum(np.abs(np.diff(rec)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(rec)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(rec)
     cum_peaks = np.sum(peaks_only)
     assert np.isclose(cum_peaks, expected_sum)
 
 
-def test_determine_peaks_only_series_with_a_double_peak_and_offset():
+def test_determine_peaks_only_delta_series_with_ground_motion():
+    record_path = TEST_DATA_DIR
+    record_filename = 'test_motion_dt0p01.txt'
+    rec = np.loadtxt(record_path + record_filename)
+    cum_abs_delta_values = np.sum(np.abs(np.diff(rec)))
+    expected_sum = cum_abs_delta_values
+    delta_peaks_only = eqsig.determine_peaks_only_delta_series(rec)
+    cum_peaks = np.sum(np.abs(delta_peaks_only))
+    assert np.isclose(cum_peaks, expected_sum), (cum_peaks, expected_sum)
+
+
+def test_determine_pseudo_cyclic_peak_only_series_with_a_double_peak_and_offset():
     values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0]) + 4
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
     cum_peaks = np.sum(peaks_only)
     expected_series = np.array([0,  2, -1,  2,  0,  1,  0,  1,  0,  1,  0])
     assert np.sum(np.abs(peaks_only - expected_series)) == 0.0
+    assert np.isclose(cum_peaks, expected_sum)
+
+
+def test_determine_peaks_only_delta_series_with_a_double_peak_and_offset():
+    values = np.array([0, 2, 1, 2, 0, 1, 0, -1, 0, 1, 0]) + 4
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    expected_sum = cum_abs_delta_values
+    delta_peaks_only = eqsig.determine_peaks_only_delta_series(values)
+    cum_peaks = np.sum(np.abs(delta_peaks_only))
+    expected_series = np.array([0,  2, -1,  1,  -2,  1,  0,  -2,  0,  2,  -1])
+    assert np.sum(np.abs(delta_peaks_only - expected_series)) == 0.0
+    assert np.isclose(cum_peaks, expected_sum)
+
+
+def test_determine_pseudo_cyclic_peak_only_series_with_non_zero_end():
+    end_value = 1.
+    values = np.array([0, 2, -1, 2, 0, end_value])
+    cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
+    expected_sum = cum_abs_delta_values / 2 + end_value / 2
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
+    cum_peaks = np.sum(peaks_only)
     assert np.isclose(cum_peaks, expected_sum)
 
 
@@ -53,10 +106,10 @@ def test_determine_peaks_only_series_with_non_zero_end():
     end_value = 1.
     values = np.array([0, 2, -1, 2, 0, end_value])
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
-    expected_sum = cum_abs_delta_values / 2 - end_value / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
-    cum_peaks = np.sum(peaks_only)
-    assert np.isclose(cum_peaks, expected_sum)
+    expected_sum = cum_abs_delta_values
+    delta_peaks_only = eqsig.determine_peaks_only_delta_series(values)
+    cum_peaks = np.sum(np.abs(delta_peaks_only))
+    assert np.isclose(cum_peaks, expected_sum), (cum_peaks, expected_sum)
 
 
 def test_determine_peaks_only_series_with_nonchanging_values():
@@ -64,7 +117,7 @@ def test_determine_peaks_only_series_with_nonchanging_values():
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
 
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
     cum_peaks = np.sum(peaks_only)
     print(cum_peaks, expected_sum)
     assert np.isclose(cum_peaks, expected_sum), cum_peaks
@@ -72,7 +125,7 @@ def test_determine_peaks_only_series_with_nonchanging_values():
     values = np.array([0, 1, 1, 3, -5, 0])  # constant the no reverse
     cum_abs_delta_values = np.sum(np.abs(np.diff(values)))
     expected_sum = cum_abs_delta_values / 2
-    peaks_only = eqsig.determine_delta_peak_only_series(values)
+    peaks_only = eqsig.determine_pseudo_cyclic_peak_only_series(values)
     cum_peaks = np.sum(peaks_only)
     assert np.isclose(cum_peaks, expected_sum), cum_peaks
 
@@ -109,7 +162,8 @@ def test_fa_spectrum_conversion():
 
 
 if __name__ == '__main__':
-    test_fa_spectrum_conversion()
+    test_determine_peaks_only_series_with_non_zero_end()
+    # test_fa_spectrum_conversion()
     # test_determine_peaks_only_series_with_sine_wave()
     # test_determine_peaks_only_series_with_triangle_series()
     # test_determine_peaks_only_series_with_ground_motion()
