* Improved speed of surface energy calculation `calc_surface_energy` and returns correct size based on input dimensions - complete

eqsig/displacements.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.integrate
 
 
-def velocity_and_displacement_from_acceleration(acceleration, dt, trap=True):
+def calc_velo_and_disp_from_accel_arr(acceleration, dt, trap=True):
     """
     Computes the velocity and acceleration of an acceleration time series, using numerical integration.
 
@@ -24,13 +24,14 @@ def velocity_and_displacement_from_acceleration(acceleration, dt, trap=True):
     """
 
     if trap is False:
-        vel_inc = acceleration * dt
-        velocity = np.cumsum(vel_inc)
-        np.insert(velocity, 0, 0)
+        velocity = np.zeros(len(acceleration) + 1)
+        velocity[1:] = acceleration * dt  # computes the increments
+        np.cumsum(velocity, out=velocity)  # passed into original array for efficiency
+        # np.insert(velocity, 0, 0)
+        # velocity = velocity[:-1]
+        displacement = velocity * dt  # computes the increments
+        np.cumsum(displacement, out=displacement)
         velocity = velocity[:-1]
-        disp_inc = velocity * dt
-        displacement = np.cumsum(disp_inc)
-        np.insert(displacement, 0, 0)
         displacement = displacement[:-1]
     else:
 
@@ -38,3 +39,26 @@ def velocity_and_displacement_from_acceleration(acceleration, dt, trap=True):
         displacement = scipy.integrate.cumtrapz(velocity, dx=dt, initial=0)
 
     return velocity, displacement
+
+
+def velocity_and_displacement_from_acceleration(acceleration, dt, trap=True):
+    """
+    Computes the velocity and acceleration of an acceleration time series, using numerical integration.
+
+    Parameters
+    ----------
+    acceleration: array_like
+        acceleration time series
+    dt: float
+        time step
+    trap: bool (default=True)
+        if True then uses trapezium integration
+
+    Returns
+    -------
+    velocity: array_like (len=len(acceleration))
+        velocity time series
+    displacement: array_like (len=len(acceleration))
+        displacement time series
+    """
+    return calc_velo_and_disp_from_accel_arr(acceleration, dt, trap=trap)

eqsig/im.py
@@ -45,8 +45,7 @@ def calc_sig_dur_vals(motion, dt, start=0.05, end=0.95, se=False):
     tuple (start_time, end_time)
     """
 
-    acc2 = motion ** 2
-    cum_acc2 = np.cumsum(acc2)
+    cum_acc2 = np.cumsum(motion ** 2)
     ind2 = np.where((cum_acc2 > start * cum_acc2[-1]) & (cum_acc2 < end * cum_acc2[-1]))
     start_time = ind2[0][0] * dt
     end_time = ind2[0][-1] * dt
@@ -126,8 +125,7 @@ def calc_sir(acc_sig):
 
 
 def _raw_calc_arias_intensity(acc, dt):
-    acc2 = acc ** 2
-    return np.pi / (2 * 9.81) * scipy.integrate.cumtrapz(acc2, dx=dt, initial=0)
+    return np.pi / (2 * 9.81) * scipy.integrate.cumtrapz(acc ** 2, dx=dt, initial=0)
 
 
 def calc_arias_intensity(acc_sig):

eqsig/surface.py
@@ -77,11 +77,13 @@ def calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., 
     # if not hasattr(down_red, '__len__'):
     #     down_red = down_red * np.ones(len(travel_times))
     if not hasattr(travel_times, '__len__'):
+        travel_times = np.array([travel_times])
+    else:
         travel_times = np.array(travel_times)
-    shifts = np.array(2 * travel_times / asig.dt)
+    shifts = 2 * travel_times / asig.dt
     max_shift = int(np.max(shifts))
     up_wave = np.pad(asig.values, (0, max_shift), mode='constant', constant_values=0)
-    dshifted = np.arange(asig.npts + max_shift)[np.newaxis, :] - shifts[:, np.newaxis]
+    dshifted = np.arange(asig.npts + max_shift)[np.newaxis, :] - shifts[:, np.newaxis]  # TODO: not needed if shifts is scalar
     down_waves = np.interp(dshifted, np.arange(asig.npts), asig.values, left=0)
     if hasattr(up_red, '__len__'):
         up_wave = up_wave[np.newaxis, :] * up_red[:, np.newaxis]  # 1d
@@ -95,7 +97,11 @@ def calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., 
         acc_series = down_waves + up_wave
     velocity = scipy.integrate.cumtrapz(acc_series, dx=asig.dt, initial=0, axis=1)
     e = 0.5 * velocity * np.abs(velocity)
-    return trim_to_length(e, asig.npts, travel_times, asig.dt, trim=trim, start=start, s2s_travel_time=stt)
+    e = trim_to_length(e, asig.npts, travel_times, asig.dt, trim=trim, start=start, s2s_travel_time=stt)
+    if len(travel_times) == 1:
+        return e[0]
+    else:
+        return e
 
 
 # def dep_calc_surface_energy(asig, travel_times, nodal=True, up_red=1, down_red=1, stt=0.0, trim=False, start=False):
@@ -140,9 +146,8 @@ def calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., 
 def calc_cum_abs_surface_energy(asig, travel_times, nodal=True, up_red=1, down_red=1, stt=0.0, trim=False, start=False):
     energy = calc_surface_energy(asig, travel_times, nodal=nodal, up_red=up_red, down_red=down_red, stt=stt,
                                  trim=trim, start=start)
-    diff = np.zeros_like(energy)
-    diff[:, 1:] = np.diff(energy, axis=1)
-    return np.cumsum(np.abs(diff), axis=1)
+    diff = np.diff(energy, axis=-1, prepend=0)
+    return np.cumsum(np.abs(diff), axis=-1)
 
 
 if __name__ == '__main__':

tests/test_surface.py
@@ -185,11 +185,15 @@ def skip_plot2():
 def test_calc_surface_energy_array_sizing():
     asig = eqsig.AccSignal(np.arange(10), dt=0.5)
     travel_times = np.array([0.0, 0.5])
-    vals = eqsig.surface.calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
-    vshape = vals.shape
+    vals0 = eqsig.surface.calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
+    vshape = vals0.shape
     assert vshape[0] == 2
     assert vshape[1] == 10
-    asig = eqsig.AccSignal(np.arange(10), dt=0.5)
+    cvals0 = eqsig.surface.calc_cum_abs_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
+    vshape = cvals0.shape
+    assert vshape[0] == 2
+    assert vshape[1] == 10
+
     travel_times = np.array([0.0, 0.5])
     up_red = np.array([1., 0.9])
     down_red = np.array([1., 0.9])
@@ -198,11 +202,23 @@ def test_calc_surface_energy_array_sizing():
     vshape = vals.shape
     assert vshape[0] == 2
     assert vshape[1] == 10
-    # travel_times = 0.4
-    # vals = eqsig.surface.calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
-    # vshape = vals.shape
-    # assert vshape[0] == 2
-    # assert vshape[1] == 10
+    vals = eqsig.surface.calc_cum_abs_surface_energy(asig, travel_times, nodal=True, up_red=up_red, down_red=down_red,
+                                             stt=0.0, trim=True)
+    vshape = vals.shape
+    assert vshape[0] == 2
+    assert vshape[1] == 10
+
+    travel_times = 0.5
+    vals2 = eqsig.surface.calc_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
+    vshape = vals2.shape
+    assert vshape[0] == 10
+    assert len(vshape) == 1
+    cvals2 = eqsig.surface.calc_cum_abs_surface_energy(asig, travel_times, nodal=True, up_red=1., down_red=1., stt=0.0, trim=True)
+    vshape = cvals2.shape
+    assert vshape[0] == 10
+    assert len(vshape) == 1
+    assert np.isclose(np.sum(np.abs(vals0[1] - vals2)), 0.0)
+    assert np.isclose(np.sum(np.abs(cvals0[1] - cvals2)), 0.0)
 
 
 if __name__ == '__main__':